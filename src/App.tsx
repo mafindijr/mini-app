5c35018f6ba8c1709bc59668ec6e601477fb4242
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-21 09:38:32 -0700

    working on side bar test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import DashboardLayout from "./layouts/DashboardLayout";
+import DashboardHome from "./pages/DashboardHome";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
 
 function App() {
-
-
   return (
-    <>
-      <div className="bg-gray-400">
-        hello
-      </div>
-    </>
-  )
+    <Routes>
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<DashboardHome />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
